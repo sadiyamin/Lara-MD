5620c4f2781c307d3272bad99c54ce5b5e6db15b
Emmanuel202320 <emmanuelbillions12345@gmail.com>
2024-12-30 01:47:30 +0100

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "", // Add your session id
+SESSION_ID: process.env.SESSION_ID || "anYXWR7L#4mq2oj8YJzS3h6CSSnz0nBv5raosDfaxbGOMjwKuQug", // Add your session id
 PREFIX: process.env.PREFIX || ".",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 MODE: process.env.MODE || "public",
